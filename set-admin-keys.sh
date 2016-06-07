00e1b49ee6199dbcfd78318a2ae1a8055866bd0a
Daniel Miller <dmiller@dimagi.com>
2016-06-07 11:58:27 -0400

    Fix permissions in set-admin-keys.sh
    Some files in /var/lib/riak were ending up owned by root, possibly because riak-admin was run as root.
    
    Update: while this still seems to work, the change was probably not necessary. The real problem seems to be docker os Mac OS X has problems with permissions. See https://github.com/boot2docker/boot2docker/issues/581#issuecomment-220282607
    
    One thing done here that is pretty important is to only set user-exists if user creation succeeds (I'm assuming the erl command exits with an error code when user creation fails).

--- set-admin-keys.sh
+++ set-admin-keys.sh
@@ -2,15 +2,16 @@
 # set insecure admin key/secret for dev/testing
 # key:      admin-key
 # secret:   admin-secret
-set -e
+sudo -u riak bash << EOF
 
 if [ ! -f /var/lib/riak/user-exists ]; then
     riak-admin wait-for-service riak_kv
 
     # https://github.com/basho/riak_cs/issues/1074#issuecomment-111110461
-    sudo -u riak $(ls -d /usr/lib/riak-cs/erts*)/bin/erl -name n@127.0.0.1 -setcookie riak \
+    $(ls -d /usr/lib/riak-cs/erts*)/bin/erl -name n@127.0.0.1 -setcookie riak \
         -eval "io:format(\"~p\", [rpc:call('riak-cs@127.0.0.1', riak_cs_user, create_user, [\"name\", \"dev@admin.com\", \"admin-key\",  \"admin-secret\"])])." \
-        -s init stop -noshell > /var/log/riak/user-creation.log 2>&1
-
-    touch /var/lib/riak/user-exists
+        -s init stop -noshell >> /var/log/riak/user-creation.log 2>&1 && \
+        touch /var/lib/riak/user-exists
 fi
+
+EOF
