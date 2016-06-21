bc20569a6139035837ae1e13d5f779fb66ee22fb
Daniel Miller <dmiller@dimagi.com>
2016-06-21 17:02:20 -0400

    More reliable user creation
    There could still be a race condition if an external process tries to communicate with riak-cs before the user is created.

--- set-admin-keys.sh
+++ set-admin-keys.sh
@@ -2,16 +2,40 @@
 # set insecure admin key/secret for dev/testing
 # key:      admin-key
 # secret:   admin-secret
-sudo -u riak bash << EOF
-
-if [ ! -f /var/lib/riak/user-exists ]; then
-    riak-admin wait-for-service riak_kv
+if [ -f /var/log/riak/user-exists ]; then
+    echo "user exists"
+else
+    host=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+    port=9980
+    counter=0
+    echo "Waiting for riak-cs at ${host}:${port}..."
+    while ! { exec 6<>/dev/tcp/${host}/${port}; } 2>/dev/null
+    do
+        sleep 1
+        let counter=counter+1
+        if [ $counter -gt 30 ]; then
+            echo "Timeout."
+            exit 1
+        fi
+    done
 
     # https://github.com/basho/riak_cs/issues/1074#issuecomment-111110461
-    $(ls -d /usr/lib/riak-cs/erts*)/bin/erl -name n@127.0.0.1 -setcookie riak \
-        -eval "io:format(\"~p\", [rpc:call('riak-cs@127.0.0.1', riak_cs_user, create_user, [\"name\", \"dev@admin.com\", \"admin-key\",  \"admin-secret\"])])." \
-        -s init stop -noshell >> /var/log/riak/user-creation.log 2>&1 && \
-        touch /var/lib/riak/user-exists
-fi
+    sudo -u riak $(ls -d /usr/lib/riak-cs/erts*)/bin/erl \
+        -noshell -name n@127.0.0.1 -setcookie riak \
+        -eval 'io:format("~p", [
+            rpc:call(
+                '"'"'riak-cs@127.0.0.1'"'"',
+                riak_cs_user,
+                create_user,
+                ["admin", "admin@admin.com", "admin-key",  "admin-secret"]
+            )
+        ]).' -s init stop > /var/log/riak/session-user-creation.log 2>&1
 
-EOF
+    cat /var/log/riak/session-user-creation.log >> /var/log/riak/user-creation.log
+    if grep -Eq '{ok,|,user_already_exists}' /var/log/riak/session-user-creation.log; then
+        touch /var/log/riak/user-exists
+    else
+        echo "admin key not set"
+        cat /var/log/riak/session-user-creation.log
+    fi
+fi
